d88c6534d7dec3a7836b0249e0b7e1379209a61c
VLiamin <LyaminVA@yandex.ru>
2019-10-25 17:07:42 +0300

    added screen saver

---
+++ VProject/HellowWords/app/src/main/java/com/example/hellowwords/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.hellowwords;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5 * 1000);
+    }
+}
