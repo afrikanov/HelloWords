3d5df116b756ce09c0a1fc2ebacb1c092a87dc25
afrikanov <barcelona1405@mail.ru>
2019-11-28 20:59:52 +0300

    new menu

--- A_dev/HelloWords/app/src/main/java/com/example/hellowords/MainActivity.java
+++ A_dev/HelloWords/app/src/main/java/com/example/hellowords/MainActivity.java
@@ -1,21 +1,35 @@
 package com.example.hellowords;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private DBHelper mDBHelper;
     private SQLiteDatabase mDB;
+    BottomNavigationItemView itemMain;
+    BottomNavigationItemView itemStatistics;
+    BottomNavigationItemView itemSettings;
+    BottomNavigationView bottomMenu;
+    LinearLayout mainActivity;
+    LinearLayout learnActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +47,46 @@ public class MainActivity extends AppCompatActivity {
         } catch (SQLException mSQLException) {
             throw mSQLException;
         }
+
+        bottomMenu = findViewById(R.id.bottom_menu);
+        itemMain = findViewById(R.id.action_main);
+        itemSettings = findViewById(R.id.action_settings);
+        itemStatistics = findViewById(R.id.action_statistics);
+
+        mainActivity = findViewById(R.id.mainActivity);
+        learnActivity = findViewById(R.id.learnActivity);
+
+        mainActivity.setVisibility(View.VISIBLE);
+        learnActivity.setVisibility(View.GONE);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.action_main);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_main:
+                                //startActivity(new Intent(MainActivity.this, MainActivity.class));
+                                mainActivity.setVisibility(View.VISIBLE);
+                                learnActivity.setVisibility(View.GONE);
+                                return true;
+                            case R.id.action_statistics:
+                                mainActivity.setVisibility(View.GONE);
+                                learnActivity.setVisibility(View.VISIBLE);
+                                //startActivity(new Intent(MainActivity.this, ));
+                                return true;
+                            case R.id.action_settings:
+                                //loadFragment(NotificationsFragment.newInstance());
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+
     }
 
     public void openCounter(View view) {
